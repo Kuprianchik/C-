2be52c5a17286cdb1c7300f07a617a0f8c4615b9
Kuprianchik.oleg <kuprianchik.oleg@gmail.com>
2024-01-24 14:13:32 +0300

    Добавил 9.1#

---
+++ 9.1 Знакомство со свойствами/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1_Знакомство_со_свойствами
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //создание обьекта 
+            MyClass obj = new MyClass(1, 9);
+            //проверка значения свойства:
+            Console.WriteLine("Свойство code: " + obj.code);
+            //присваивание значения свойству:
+            obj.code = 7;
+            //проверка значения свойства:
+            Console.WriteLine("Свойство code: " + obj.code);
+            //присваивание значения свойству:
+            obj.code = 20;
+            //проверка значения свойства:
+            Console.WriteLine("Свойство code: " + obj.code);
+            //присваивание значения свойству:
+            obj.code = -10;
+            //проверка значения свойства:
+            Console.WriteLine("Свойство code: " + obj.code);
+
+        }
+    }
+    class MyClass
+    {
+        //закрытые целочисленные поля
+        private int num;
+        private int min;
+        private int max;
+        public MyClass(int a, int b)
+        {
+            //присваивание значения полям:
+            min = a;
+            max = b;
+            //присваивание значения свойству:
+            code = (max + min) / 2;
+        }
+        //описание целочисленного свойства:
+        public int code
+        {
+            //метод вызывается при считывании свойства:
+            get
+            {
+                //значение свойства
+                return num;
+            }
+            set
+            {
+                //Если присваиваемое значение меньше минимально
+                //допустимого
+                if (value < min)
+                    num = min;
+                //если присваиваемое значение больше максимально допустимого
+                else if (value > max)
+                    num = max;
+                else num = value;
+            }
+        }
+    }
+}
---
+++ 9.1 Знакомство со свойствами/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("9.1 Знакомство со свойствами")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9.1 Знакомство со свойствами")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("04c242b7-c7d0-4df9-b3b8-6f653f124fc3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
