2a4a02ef764c30d341dd72fa698f0762d63703dd
Oleg <kuprianchik.oleg@gmail.com>
2024-02-04 16:34:33 +0300

    дополнил 9.15

--- 9.15 перегрузка индексаторов/Program.cs
+++ 9.15 перегрузка индексаторов/Program.cs
@@ -14,10 +14,10 @@ namespace _9._15_перегрузка_индексаторов
             MyClass obj = new MyClass(6);
             Console.WriteLine(obj);
             //поэлементное отображение содержимого массива
-            for(int k=0;k<n+3;k++ ) 
+            for (int k = 0; k < n + 3; k++)
             {
                 //объект с целочисленным индексом
-                Console.WriteLine(obj[k]+" ");
+                Console.WriteLine(obj[k] + " ");
             }
             Console.WriteLine("\n");
             //объект с целочисленным индексом:
@@ -25,10 +25,31 @@ namespace _9._15_перегрузка_индексаторов
             obj[n + 3] = 8;
             Console.WriteLine(obj);
             //объект с символьным индексом:
-            obj['a']=9;
+            obj['a'] = 9;
             obj['k'] = 0;
             //проверка
             Console.WriteLine(obj);
+            Console.WriteLine("Проверка:");
+            //поэлементное отображение содержимого массива:
+            for (char s = 'a'; s < 'a' + n + 3; s++)
+            {
+                //обьект с символьным индексом
+                Console.Write(obj[s] + " ");
+            }
+            Console.WriteLine("\n");
+            //Объект с целочисленным и текстовым индексом
+            obj[4, "alpha"] = 0;
+            obj["bravo", 0] = 6;
+            //проверка
+            Console.WriteLine(obj);
+            //текстовая переменная
+            string txt = "abc";
+            Console.WriteLine("Проверка:");
+            for(int k=0;k<txt.Length;k++)
+            {
+                //объект с двумя индексами
+                Console.WriteLine(obj[k,txt]+": " + obj[txt,k]);
+            }
         }
     }
     class MyClass
@@ -38,9 +59,9 @@ namespace _9._15_перегрузка_индексаторов
         public MyClass(int size)
         {
             //создание массива
-            nums=new int[size];
+            nums = new int[size];
             //заполнение массива
-            for(int k=0; k<nums.Length; k++)
+            for (int k = 0; k < nums.Length; k++)
             {
                 //использование индексатора
                 this[k] = k + 1;
@@ -49,7 +70,7 @@ namespace _9._15_перегрузка_индексаторов
         public override string ToString()
         {
             string txt = "Содержимое объекта:\n";
-            for(int k=0;k<nums.Length;k++)
+            for (int k = 0; k < nums.Length; k++)
             {
                 //используется индексатор с целочисленным
                 //индексом
@@ -82,11 +103,11 @@ namespace _9._15_перегрузка_индексаторов
             {
                 //используется индексатор с целочисленным 
                 //индексом
-                this[s-'a'] = value;
+                this[s - 'a'] = value;
             }
         }
         //индексатор с целочисленным и текстовым индексом
-        public int this[int k,string t]
+        public int this[int k, string t]
         {
             get
             {
@@ -100,7 +121,7 @@ namespace _9._15_перегрузка_индексаторов
             }
         }
         //индексатор с текстовым и целочисленным индексом
-        public int this[string t,int k]
+        public int this[string t, int k]
         {
             get
             {
