daad5f77909fbae8bb61763d525527f604b705b1
Kuprianchik.oleg <kuprianchik.oleg@gmail.com>
2024-03-12 10:43:14 +0300

    Добавил домашние задания, 1 и 2. Первое пока что не работает

---
+++ 2.1 домашнее задание 1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace _2._1_домашнее_задание_1
+{
+    abstract class Base
+    {
+        protected int[] mas;
+        public Base(int R)
+        {
+           // mas = new int[R];
+        }
+        abstract public int R
+        {
+            set;
+            get;
+        }
+        public abstract void show();
+        abstract public int this[int i] { get; set; }
+    }
+    class First:Base
+    {
+        public First(int n):base(n)
+        {
+            R = n;
+            mas= new int[R];
+        }
+
+        public override int R
+        {
+            set
+            {
+                R = value;
+            }
+            get
+            {
+                return R;
+            }
+        }
+        public override void show()
+        {
+            for(int i=0;i<R;i++)
+            {
+                Console.Write("| " + mas[i]+" |");
+            }
+        }
+        public override int this[int i]
+        {
+            set
+            {
+                mas[i] = value;
+            }
+            get
+            {
+                return mas[i];
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            First obj = new First(5);
+           
+            for(int i=0;i<5;i++)
+            {
+                obj[i] = 2 * i;
+            }
+            obj.show();
+        }
+    }
+}
---
+++ 2.1 домашнее задание 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1_домашнее_задание_2
+{
+    abstract class Base
+    {
+        protected string pole;
+        public  Base(string txt)
+        {
+         pole = txt;
+        }
+        abstract public int n
+        {
+            get;
+        }
+        abstract public void show(string t);
+        abstract public void show();
+        abstract public int this[int i]
+        {
+            get;
+        }
+    }
+    class First:Base
+    {
+        public First(string txt):base(txt)
+        {
+
+        }
+        public override int n
+        {
+            get
+            {
+                return pole.Length;
+            }
+        }
+        public override void show()
+        {
+            Console.WriteLine(pole);
+        }
+        public override void show(string t)
+        {
+            pole = t;
+        }
+        public override int this[int i]
+        {
+            get { return (int)pole[i]; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            First obj = new First("Александра");
+            obj.show();
+            Console.WriteLine(obj.n);
+            Console.WriteLine(obj[1]);
+            obj.show("Митрахович");
+            obj.show();
+        }
+    }
+}
