6d8b772f30b74a938a5138e1e7c1cf4889de4ce6
Kuprianchik.oleg <kuprianchik.oleg@gmail.com>
2024-03-14 16:54:10 +0300

    доделал 9 задание дом

--- 2.1 домашнее задание 9/Program.cs
+++ 2.1 домашнее задание 9/Program.cs
@@ -11,29 +11,66 @@ namespace _2._1_домашнее_задание_9
     abstract class Base
     {
         protected char[] mas;
-         public Base(string txt)
+        public Base(string txt)
         {
-            mas=txt.ToArray();
+            mas = txt.ToArray();
         }
         public char Show(int n)
         {
             return mas[n];
         }
-        public abstract void Show(int n, char c);  
+        public abstract void Show(int n, char c);
     }
     interface First
     {
         void Show(string txt);
-        char this[int n,int g] { get; }
+        char this[int n] { get; set; }
     }
     class Boss : Base, First
     {
-
+        char[] mas1;
+        public Boss(string txt):base(txt)
+        { }
+        public override void Show(int n, char c)
+        {
+            mas[n] = c;
+        }
+        //создание нового массива 
+        public void Show(string txt)
+        {
+            mas1 = txt.ToArray();
+            mas = mas1;
+        }
+        //получение элемента, и установка нового
+        public char this[int n]
+        {
+            get
+            {
+                return (char)mas[n];
+            }
+            set
+            {
+                Show(n, value);
+            }
+        }
+        public string Show()
+        {
+            //преобразование в текстовую строку и возврат
+            return string.Concat<char>(mas);
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Boss obj = new Boss("Александра");
+            Console.WriteLine("Индексатор: " + obj[5]);
+            Console.WriteLine(obj[5]='ы') ;
+            Console.WriteLine(obj.Show());
+            obj.Show("Митрахович-молодец");
+            Console.WriteLine(obj.Show());
+
+
         }
     }
 }
