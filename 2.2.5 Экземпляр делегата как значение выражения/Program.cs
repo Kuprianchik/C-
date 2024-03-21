3c7b1db9c3425119cc088ac5d009910c19c10afc
Kuprianchik.oleg <kuprianchik.oleg@gmail.com>
2024-03-21 08:33:39 +0300

    33

---
+++ 2.2.5 Экземпляр делегата как значение выражения/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._5_Экземпляр_делегата_как_значение_выражения
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
