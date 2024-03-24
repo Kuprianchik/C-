7536e53ae53dac2a6ba33be4f4be4ae2bb5f7d5d
Oleg <kuprianchik.oleg@gmail.com>
2024-03-24 18:00:20 +0300

    решил 2 домашних задания 2 главы

---
+++ 2.2 домашнее задание 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//объявление делегата
+delegate int MyDelegate(char s, string str);
+namespace _2._2_домашнее_задание_1
+{
+    internal class Program
+    {
+        static int First(char s,string str)
+        {
+            int n=0;
+            for(int i=0;i<str.Length;i++)
+            {
+                if (s == str[i])
+                {
+                    n++;
+                }
+            }
+            return n;
+        }
+        static int Second(char s,string str)
+        {
+            int n=0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (s == str[i])
+                {
+                    n=i+1;
+                    break;
+                }
+            }
+            if(n!=0)
+            {
+                return n;
+            }
+            else
+            {
+                return -1;
+            }    
+        }
+        static void Main(string[] args)
+        {
+            MyDelegate md;
+            md = Second;
+            Console.WriteLine(md('ы',"Александра"));
+            md = First;
+            Console.WriteLine(md('е',"Александра"));
+        }
+    }
+}
---
+++ 2.2 домашнее задание 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//объявление делегата
+delegate void MyDelegate(char s);
+//объявление класса
+class Alpha
+{
+    private char s;
+    public void Set(char s)
+    {
+        this.s = s;
+        Console.WriteLine("Присвоили значение: "+s);
+    }
+}
+namespace _2._2_домашнее_задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alpha[] alpha = new Alpha[5];
+            MyDelegate md;
+         
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                alpha[i]= new Alpha();
+            }
+            md = alpha[0].Set;
+            for(int i=1; i < alpha.Length; i++)
+            {
+                md += alpha[i].Set;
+                md((char)('a' + i));
+            }
+
+        }
+
+    }
+}
