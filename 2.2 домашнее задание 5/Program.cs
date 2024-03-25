03e5e199e255a2392318f425411aaca048b86c7b
Oleg <kuprianchik.oleg@gmail.com>
2024-03-25 22:29:14 +0300

    попытка 5 задачи

--- 2.2 домашнее задание 5/Program.cs
+++ 2.2 домашнее задание 5/Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //объявление делегата
 delegate int MyDelegate();
 //объявление класса
+class MyClass
+{
+    private static int sum;
+    public int GetSum()
+    {
+        return sum;
+    }
+    public MyDelegate md;
 
+
+}
 namespace _2._2_домашнее_задание_5
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            MyClass A = new MyClass();
+            A.md = delegate ()
+            {
+                return A.GetSum();
+            };
         }
     }
 }
